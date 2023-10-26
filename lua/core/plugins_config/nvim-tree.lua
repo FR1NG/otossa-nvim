c25f0cdbc9e5e4e68fce945ad2b81b10e452fb72
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-26 15:09:56 +0100

    adding keymaps for nvim-tree

--- lua/core/plugins_config/nvim-tree.lua
+++ lua/core/plugins_config/nvim-tree.lua
@@ -1,7 +1,60 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+  local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
 
-require("nvim-tree").setup()
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set("n", "l", api.node.open.edit, opts "Open")
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close Directory")
+    vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
+    vim.keymap.del("n", "<C-k>", { buffer = bufnr })
+    vim.keymap.set("n", "<S-k>", api.node.open.preview, opts "Open Preview")
+  end
 
 
-vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<cr>')
+  require("nvim-tree").setup {
+    on_attach = my_on_attach,
+    hijack_netrw = false,
+    sync_root_with_cwd = true,
+    renderer = {
+      add_trailing = false,
+      group_empty = false,
+      highlight_git = false,
+      full_name = false,
+      highlight_opened_files = "none",
+      root_folder_label = ":t",
+      indent_width = 2,
+      indent_markers = {
+        enable = false,
+        inline_arrows = true,
+        icons = {
+          corner = "└",
+          edge = "│",
+          item = "│",
+          none = " ",
+        },
+      },
+      special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+      symlink_destination = true,
+    },
+    update_focused_file = {
+      enable = true,
+      debounce_delay = 15,
+      update_root = true,
+      ignore_list = {},
+    },
+
+    diagnostics = {
+      enable = true,
+      show_on_dirs = false,
+      show_on_open_dirs = true,
+      debounce_delay = 50,
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+        max = vim.diagnostic.severity.ERROR,
+      },
+    },
+  }
