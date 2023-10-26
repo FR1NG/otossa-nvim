f52cd56eb44250e8dade7756473a6c3fa66f2cde
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-26 15:37:03 +0100

    using system clipboard on nvim

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = ' '
+vim.wo.number = true
+vim.opt.clipboard="unnamed,unnamedplus"
+
 vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<cr>')
 vim.keymap.set('n', '<leader>q', ':q<cr>')
 vim.keymap.set('n', '<leader>w', ':w<cr>')
@@ -7,3 +10,4 @@ vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<C-h>', '<C-W>h')
 vim.keymap.set('n', '<C-l>', '<C-W>l')
 vim.keymap.set('i', 'jj', '<Esc>')
+
