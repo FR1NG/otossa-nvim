c5c39dc6ef41184976f76467695a3b936bb247c5
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-10-27 22:12:01 +0100

    using tab and shift-tab to select next and prev in cmp menu

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -22,10 +22,12 @@ M.config = function()
 			end,
 		},
 		window = {
-			-- completion = cmp.config.window.bordered(),
-			-- documentation = cmp.config.window.bordered(),
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
+    			['<Tab>'] = cmp.mapping.select_next_item(),
+    			['<S-Tab>'] = cmp.mapping.select_prev_item(),
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
